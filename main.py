ce6d752b1b8b360e431a45b6d20b65b8b023fd27
Sam Vutheareak <sam.vutheareak@FA-993-601-1082.local>
2025-12-11 14:32:04 +0700

    fix: FastAPI routes with unique function names

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/a")
-def root():
+def get_root():
     return {"service": "service-a", "status": "running"}
 
 @app.get("/a/health")
-def health():
+def get_health():
     return {"status": "ok"}
 
 @app.get("/a/info")
-def info():
+def get_info():
     return {"service": "service-a", "path": "/a/info", "status": "running"}
 
 @app.get("/a/info/g3")
-def info():
-    return {"service": "service-a", "path": "/a/info", "status": "running", "group": "devops-team-3"}
+def get_info_g3():
+    return {"service": "service-a", "path": "/a/info/g3", "status": "running", "group": "devops-team-3"}
 
 @app.get("/a/info/g4")
-def info():
-    return {"service": "service-a", "path": "/a/info", "status": "running", "group": "devops-team-4"}
+def get_info_g4():
+    return {"service": "service-a", "path": "/a/info/g4", "status": "running", "group": "devops-team-4"}
